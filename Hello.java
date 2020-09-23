9195c26f1ad018fb9c09f7d85d35e8d54da32821
santiagocatolica <santiago.negreira@catolicasc.org.br>
2020-09-22 21:24:32 -0300

    Primeiro programa java

---
+++ Hello.java
@@ -0,0 +1,12 @@
+public class Hello
+{
+	public static void main(String[] args)
+	{
+		System.out.print("Esta é uma saudação: ");
+		System.out.println("Hello, world!");
+		System.out.printf("Idade: %d\n", 20);
+	}
+}
+
+// javac Hello.java - compila para bytecode (.class)
+// java Hello - executa o arquivo .class
